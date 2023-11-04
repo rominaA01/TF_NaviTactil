542aae88287539651d40af0b9523fc7660122664
rominaA01 <117952365+rominaA01@users.noreply.github.com>
2023-11-03 19:48:29 -0500

    Add files via upload

---
+++ scriptLP.js
@@ -0,0 +1,16 @@
+
+function Res1(){
+    document.querySelector(".resenia3").style.display = "none";
+    document.querySelector(".resenia4").style.display = "none";
+
+    document.querySelector('.resenia1').style.display = "block";
+    document.querySelector('.resenia2').style.display = "block";
+}
+
+function Res2(){
+    document.querySelector(".resenia1").style.display = "none";
+    document.querySelector(".resenia2").style.display = "none";
+
+    document.querySelector('.resenia3').style.display = "block";
+    document.querySelector('.resenia4').style.display = "block";
+}
